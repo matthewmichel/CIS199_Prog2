8a0d5f435b42442d243297e544f0a69804b1427d
matthewmichel <mattmichel17@gmail.com>
2021-04-21 18:35:48 -0400

    initial commit

---
+++ Prog2/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace Prog2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lastNameLbl = new System.Windows.Forms.Label();
+            this.lastNameTxtBox = new System.Windows.Forms.TextBox();
+            this.freshmanRadio = new System.Windows.Forms.RadioButton();
+            this.sophomoreRadio = new System.Windows.Forms.RadioButton();
+            this.juniorRadio = new System.Windows.Forms.RadioButton();
+            this.seniorRadio = new System.Windows.Forms.RadioButton();
+            this.checkRegistrationBtn = new System.Windows.Forms.Button();
+            this.gradeLevelGroupBox = new System.Windows.Forms.GroupBox();
+            this.gradeLevelGroupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lastNameLbl
+            // 
+            this.lastNameLbl.AutoSize = true;
+            this.lastNameLbl.Location = new System.Drawing.Point(13, 13);
+            this.lastNameLbl.Name = "lastNameLbl";
+            this.lastNameLbl.Size = new System.Drawing.Size(163, 13);
+            this.lastNameLbl.TabIndex = 0;
+            this.lastNameLbl.Text = "Enter First Letter of LAST NAME:";
+            // 
+            // lastNameTxtBox
+            // 
+            this.lastNameTxtBox.Location = new System.Drawing.Point(182, 10);
+            this.lastNameTxtBox.Name = "lastNameTxtBox";
+            this.lastNameTxtBox.Size = new System.Drawing.Size(21, 20);
+            this.lastNameTxtBox.TabIndex = 1;
+            // 
+            // freshmanRadio
+            // 
+            this.freshmanRadio.AutoSize = true;
+            this.freshmanRadio.Location = new System.Drawing.Point(6, 21);
+            this.freshmanRadio.Name = "freshmanRadio";
+            this.freshmanRadio.Size = new System.Drawing.Size(71, 17);
+            this.freshmanRadio.TabIndex = 0;
+            this.freshmanRadio.TabStop = true;
+            this.freshmanRadio.Text = "Freshman";
+            this.freshmanRadio.UseVisualStyleBackColor = true;
+            // 
+            // sophomoreRadio
+            // 
+            this.sophomoreRadio.AutoSize = true;
+            this.sophomoreRadio.Location = new System.Drawing.Point(6, 44);
+            this.sophomoreRadio.Name = "sophomoreRadio";
+            this.sophomoreRadio.Size = new System.Drawing.Size(79, 17);
+            this.sophomoreRadio.TabIndex = 1;
+            this.sophomoreRadio.TabStop = true;
+            this.sophomoreRadio.Text = "Sophomore";
+            this.sophomoreRadio.UseVisualStyleBackColor = true;
+            // 
+            // juniorRadio
+            // 
+            this.juniorRadio.AutoSize = true;
+            this.juniorRadio.Location = new System.Drawing.Point(6, 67);
+            this.juniorRadio.Name = "juniorRadio";
+            this.juniorRadio.Size = new System.Drawing.Size(53, 17);
+            this.juniorRadio.TabIndex = 2;
+            this.juniorRadio.TabStop = true;
+            this.juniorRadio.Text = "Junior";
+            this.juniorRadio.UseVisualStyleBackColor = true;
+            // 
+            // seniorRadio
+            // 
+            this.seniorRadio.AutoSize = true;
+            this.seniorRadio.Location = new System.Drawing.Point(7, 91);
+            this.seniorRadio.Name = "seniorRadio";
+            this.seniorRadio.Size = new System.Drawing.Size(55, 17);
+            this.seniorRadio.TabIndex = 3;
+            this.seniorRadio.TabStop = true;
+            this.seniorRadio.Text = "Senior";
+            this.seniorRadio.UseVisualStyleBackColor = true;
+            // 
+            // checkRegistrationBtn
+            // 
+            this.checkRegistrationBtn.Location = new System.Drawing.Point(57, 159);
+            this.checkRegistrationBtn.Name = "checkRegistrationBtn";
+            this.checkRegistrationBtn.Size = new System.Drawing.Size(101, 44);
+            this.checkRegistrationBtn.TabIndex = 3;
+            this.checkRegistrationBtn.Text = "Check Registration Date";
+            this.checkRegistrationBtn.UseVisualStyleBackColor = true;
+            this.checkRegistrationBtn.Click += new System.EventHandler(this.checkRegistrationBtn_Click);
+            // 
+            // gradeLevelGroupBox
+            // 
+            this.gradeLevelGroupBox.Controls.Add(this.sophomoreRadio);
+            this.gradeLevelGroupBox.Controls.Add(this.seniorRadio);
+            this.gradeLevelGroupBox.Controls.Add(this.freshmanRadio);
+            this.gradeLevelGroupBox.Controls.Add(this.juniorRadio);
+            this.gradeLevelGroupBox.Location = new System.Drawing.Point(16, 36);
+            this.gradeLevelGroupBox.Name = "gradeLevelGroupBox";
+            this.gradeLevelGroupBox.Size = new System.Drawing.Size(198, 114);
+            this.gradeLevelGroupBox.TabIndex = 2;
+            this.gradeLevelGroupBox.TabStop = false;
+            this.gradeLevelGroupBox.Text = "Select Your CURRENT Grade Level";
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.checkRegistrationBtn;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(226, 213);
+            this.Controls.Add(this.checkRegistrationBtn);
+            this.Controls.Add(this.lastNameTxtBox);
+            this.Controls.Add(this.lastNameLbl);
+            this.Controls.Add(this.gradeLevelGroupBox);
+            this.Name = "Form1";
+            this.Text = "Prog 2";
+            this.gradeLevelGroupBox.ResumeLayout(false);
+            this.gradeLevelGroupBox.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lastNameLbl;
+        private System.Windows.Forms.TextBox lastNameTxtBox;
+        private System.Windows.Forms.RadioButton freshmanRadio;
+        private System.Windows.Forms.RadioButton sophomoreRadio;
+        private System.Windows.Forms.RadioButton juniorRadio;
+        private System.Windows.Forms.RadioButton seniorRadio;
+        private System.Windows.Forms.Button checkRegistrationBtn;
+        private System.Windows.Forms.GroupBox gradeLevelGroupBox;
+    }
+}
+
---
+++ Prog2/Form1.cs
@@ -0,0 +1,195 @@
+﻿/*
+ * GRADING ID: A9391
+ * PROGRAM NUMBER: 02
+ * DUE DATE: 16 October 2018
+ * COURSE SECTION: 01
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prog2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // This logic is run whenever the button is clicked. It determines the day and time of registration given the lastName and gradeLevel variables
+        private void checkRegistrationBtn_Click(object sender, EventArgs e)
+        {
+            char lastName; // stores the user inputted FIRST LETTER of their LAST NAME
+            int gradeLevel; // is determined by which radio box is selected
+            string day = "INVALID"; // will be used when showing the MessageBox. Set to INVALID as default
+            string time = "INVALID"; // will be used when showing the MessageBox. Set to INVALID as default.
+
+
+            // determines the gradeLevel based on the radio boxes
+            if (freshmanRadio.Checked == true)
+                gradeLevel = 1;
+            else if (sophomoreRadio.Checked == true)
+                gradeLevel = 2;
+            else if (juniorRadio.Checked == true)
+                gradeLevel = 3;
+            else if (seniorRadio.Checked == true)
+                gradeLevel = 4;
+            else {
+                gradeLevel = 0;
+                MessageBox.Show("You must select a grade level.");
+            }
+
+            // turns the lastNameTxtBox.Text value into a char data type
+            if (lastNameTxtBox.Text.Length == 1)
+                char.TryParse(lastNameTxtBox.Text, out lastName);
+            else
+            {
+                MessageBox.Show("Please only enter the FIRST LETTER of your LAST NAME");
+                lastName = 'a';
+            }
+
+            // makes all lastName chars lower case
+            lastName = char.ToLower(lastName);
+
+            // determines day and time if a Junior or Senior
+            if (gradeLevel == 4 || gradeLevel == 3)
+            {
+                if (gradeLevel == 4)
+                    day = "November 2nd";
+                else if (gradeLevel == 3)
+                    day = "November 5th";
+
+                if (lastName <= 'd')
+                    time = "4:00 PM";
+                else if (lastName <= 'i')
+                    time = "8:30 AM";
+                else if (lastName <= 'o')
+                    time = "10:00 AM";
+                else if (lastName <= 's')
+                    time = "11:30 AM";
+                else if (lastName <= 'z')
+                    time = "2:00 PM";
+            }
+            else if (gradeLevel == 2) // determines day and time if a sophomore
+            {
+
+                if (lastName <= 'b')
+                {
+                    day = "November 7th";
+                    time = "2:00 PM";
+                }
+                else if (lastName <= 'd')
+                {
+                    day = "November 7th";
+                    time = "4:00 PM";
+                }
+                else if (lastName <= 'f')
+                {
+                    day = "November 6th";
+                    time = "8:30 AM";
+                }
+                else if (lastName <= 'i')
+                {
+                    day = "November 6th";
+                    time = "10:00 AM";
+                }
+                else if (lastName <= 'l')
+                {
+                    day = "November 6th";
+                    time = "11:30 AM";
+                }
+                else if (lastName <= 'o')
+                {
+                    day = "November 6th";
+                    time = "2:00 PM";
+                }
+                else if (lastName <= 'q')
+                {
+                    day = "November 6th";
+                    time = "4:00 PM";
+                }
+                else if (lastName <= 's')
+                {
+                    day = "November 7th";
+                    time = "8:30 AM";
+                }
+                else if (lastName <= 'v')
+                {
+                    day = "November 7th";
+                    time = "10:00 AM";
+                }
+                else if (lastName <= 'z')
+                {
+                    day = "November 7th";
+                    time = "11:30 AM";
+                }
+            }
+            else if (gradeLevel == 1) // determines day and time if a freshman
+            {
+
+                if (lastName <= 'b')
+                {
+                    day = "November 9th";
+                    time = "2:00 PM";
+                }
+                else if (lastName <= 'd')
+                {
+                    day = "November 9th";
+                    time = "4:00 PM";
+                }
+                else if (lastName <= 'f')
+                {
+                    day = "November 8th";
+                    time = "8:30 AM";
+                }
+                else if (lastName <= 'i')
+                {
+                    day = "November 8th";
+                    time = "10:00 AM";
+                }
+                else if (lastName <= 'l')
+                {
+                    day = "November 8th";
+                    time = "11:30 AM";
+                }
+                else if (lastName <= 'o')
+                {
+                    day = "November 8th";
+                    time = "2:00 PM";
+                }
+                else if (lastName <= 'q')
+                {
+                    day = "November 8th";
+                    time = "4:00 PM";
+                }
+                else if (lastName <= 's')
+                {
+                    day = "November 9th";
+                    time = "8:30 AM";
+                }
+                else if (lastName <= 'v')
+                {
+                    day = "November 9th";
+                    time = "10:00 AM";
+                }
+                else if (lastName <= 'z')
+                {
+                    day = "November 9th";
+                    time = "11:30 AM";
+                }
+            }
+
+            // shows the final message box displaying the day and time of registration
+            MessageBox.Show($"You are able to register on {day} at {time}");
+
+        }
+    }
+}
